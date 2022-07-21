c51f5b264090c9a76443fe99117b4ca4275e151b
Kevin Bertrand <k.bertrand@desyntic.com>
2022-07-21 18:56:14 +0200

    Code refractor

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,9 +6,7 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
+    // Configure database
     if app.environment == .production {
         app.databases.use(.postgres(
             hostname: Environment.get("DATABASE_HOST") ?? "localhost",
