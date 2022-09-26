28888596820398945cb64e192784dbd1734bd283
Kevin Bertrand <73759450+kevin-bertrand@users.noreply.github.com>
2022-09-26 14:22:40 +0200

    Update PDF

--- Sources/App/Controllers/TripController.swift
+++ Sources/App/Controllers/TripController.swift
@@ -177,7 +177,10 @@ struct TripController: RouteCollection {
                tripDate <= endFilterDate && tripDate > startFilterDate {
                 let startingAddress = try await addressController.getAddressFromId(trip.$startingAddress.id, for: req)
                 let endingAddress = try await addressController.getAddressFromId(trip.$endingAddress.id, for: req)
-                tripList.append(["\(tripDate.dateOnly)", "\(trip.totalDistance.twoDigitPrecision) km", "\(startingAddress?.city ?? "No address")", "\(endingAddress?.city ?? "No address")"])
+                tripList.append(["\(tripDate.dateOnly)",
+                                 "\(startingAddress?.city ?? "No address") -> \(endingAddress?.city ?? "No address")",
+                                 "\(trip.missions.joined(separator: ", "))\(trip.comment != nil ? ("\n" + trip.comment!) : "")",
+                                 "\(trip.totalDistance.twoDigitPrecision) km"])
                 totalDistance += trip.totalDistance
             }
         }
